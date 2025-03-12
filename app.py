5a87a91dd5580bb3f12b902aaebb246498a6d8e6
UlifiPond <ulifi0110901@gmail.com>
2025-03-12 19:49:23 -0400

    minor fix

--- app.py
+++ app.py
@@ -1,38 +1,52 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import os
-
-app = Flask(__name__) # create an app instance  
-
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+import uuid
+from werkzeug.utils import secure_filename
+from PIL import Image
+app = Flask(__name__)
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['UPLOAD_FOLDER'] = os.path.join(base_dir, 'static', 'uploads')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'php', 'py'}
+app.secret_key = 'your_secret_key'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def is_valid_image(file):
+    try:
+        Image.open(file)
+        return True
+    except Exception:
+        return False
+    
 @app.route('/')
 def index():
     images = os.listdir(app.config['UPLOAD_FOLDER'])
+    print(os.listdir(app.config['UPLOAD_FOLDER']))
     return render_template('index.html', images=images)
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return redirect(request.url)
+        flash('No file Selected', 'error')
+        return redirect(url_for('index'))
     
     file = request.files['file']
     
     if file.filename == '':
-        return redirect(request.url)
-    
-    if file and allowed_file(file.filename):
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('No file Selected', 'error')
         return redirect(url_for('index'))
     
-    return redirect(request.url)
-if __name__ == '__main__':
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
+    if file and allowed_file(file.filename) and is_valid_image(file):
+        filename = secure_filename(str(uuid.uuid4()) + os.path.splitext(file.filename)[1])
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Upload Success', 'success')
+    else:
+        flash('unsupported extension or invalid content', 'error')
     
-    app.run(debug=True)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(host='0.0.0.0', port=5000, debug=True)
